a5a723205eaaf6c7469176bf1097289576b86d3a
Reabetswe <140710640+ReaDaCoder@users.noreply.github.com>
2024-10-17 15:32:46 +0200

    Added delete logic

--- AddBook.js
+++ AddBook.js
@@ -30,3 +30,17 @@ axios
   .catch(error => {
     console.error(error);
   });
+
+  const axios = require('axios');
+
+  axios
+    .post('http://localhost:3000/', {
+    
+    })
+    .then(res => {
+      console.log(`statusCode: ${res.status}`);
+      console.log(res);
+    })
+    .catch(error => {
+      console.error(error);
+    });
